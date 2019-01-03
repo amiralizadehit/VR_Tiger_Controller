b4c878cc465e118ed59efd4cc2d6f3a23470851b
Amir Alizadeh <amiralizadeh.it@gmail.com>
2019-01-03 18:35:18 +0200

    Update Property On Model Controller

--- Assets/Scripts/ModelController.cs
+++ Assets/Scripts/ModelController.cs
@@ -28,29 +28,7 @@ public class ModelController : MonoBehaviour
         else
         {
             isInitialized = true;
-            foreach (var controller in controllers)
-            {
-                controller.bone.GetComponent<Rigidbody>().mass = controller.rigidbody.mass;
-                JointLimits limit = new JointLimits()
-                {
-                    max = controller.hinge.limitMax,
-                    min = controller.hinge.limitMin,
-                    bounceMinVelocity = 0.2f,
-                    bounciness = 0
-                };
-                controller.bone.GetComponent<HingeJoint>().limits = limit;
-                controller.bone.GetComponent<HingeJoint>().useLimits = controller.hinge.useLimits;
-
-
-                JointSpring spring = new JointSpring()
-                {
-                    spring = controller.hinge.spring,
-                    damper = controller.hinge.damper,
-                    targetPosition = controller.hinge.targetPosition
-                };
-                controller.bone.GetComponent<HingeJoint>().spring = spring;
-                controller.bone.GetComponent<HingeJoint>().useSpring = controller.hinge.useSpring;
-            }
+            UpdateModel();
         }
     }
 
@@ -74,6 +52,8 @@ public class ModelController : MonoBehaviour
                         },
                         hinge = new HingJointController()
                         {
+                            axis = hinge.axis,
+
                             useLimits = hinge.useLimits,
                             limitMin = hinge.limits.min,
                             limitMax = hinge.limits.max,
@@ -92,9 +72,16 @@ public class ModelController : MonoBehaviour
 
     void Update()
     {
+        UpdateModel();   
+    }
+
+
+    private void UpdateModel()
+    {
         foreach (var controller in controllers)
         {
             controller.bone.GetComponent<Rigidbody>().mass = controller.rigidbody.mass;
+            controller.bone.GetComponent<HingeJoint>().axis = controller.hinge.axis;
             JointLimits limit = new JointLimits()
             {
                 max = controller.hinge.limitMax,
@@ -138,6 +125,8 @@ public class RigidBodyController
 [System.Serializable]
 public class HingJointController
 {
+    public Vector3 axis;
+
     public bool useLimits;
     public float limitMin;
     public float limitMax;
