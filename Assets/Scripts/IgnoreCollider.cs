d8939a5f50ac21cb81562b64e3c85f787b7fa37e
Amir Alizadeh <amiralizadeh.it@gmail.com>
2019-01-07 20:39:50 +0200

    Some Changes in Physics and the Model

---
+++ Assets/Scripts/IgnoreCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollider : MonoBehaviour
+{
+	void Start () {
+
+        Physics.IgnoreCollision(gameObject.GetComponent<BoxCollider>(),transform.parent.GetComponent<CapsuleCollider>());
+	}
+
+}
--- Assets/Scripts/ModelController.cs
+++ Assets/Scripts/ModelController.cs
@@ -1,63 +1,61 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.Networking;
 
 public class ModelController : MonoBehaviour
 {
     public float totalVolume;
+
+
     public float totalMass;
     public float modelDamper;
     public float modelSpring;
-    public List<Controller> controllers;
+
+    public bool useCustomValues;
+    //public List<Controller> controllers;
+
+    [Header("Body Parts")]
+    [NamedArrayAttribute(new string[]
+    {
+        "Waist",
+        "Neck",
+        "Right Leg Toe",
+        "Right Leg Shin",
+        "Right Leg Thigh",
+        "Right Hand Toe",
+        "Right Hand Forearm",
+        "Right Hand Arm",
+        "Left Leg Toe",
+        "Left Leg Shin",
+        "Left Leg Thigh",
+        "Left Hand Toe",
+        "left Hand Forearm",
+        "Left Hand Arm",
+        "Left Leg Upper",
+        "Right Leg Upper"
+    })]
+    public float[] parts;
 
 
-    private GameObject[] bones;
-    private Rigidbody body;
-    private HingeJoint hinge;
-    private CapsuleCollider collider;
-    private bool isInitialized;
+    public GameObject root;
+    public GameObject[] bones;
 
 
     void Awake()
     {
-        bones = GameObject.FindGameObjectsWithTag("Bone");
-        if (controllers.Count == 0)
-            controllers = new List<Controller>(bones.Length);
-        else
-        {
-            isInitialized = true;
-            UpdateModel();
-        }
+        Time.fixedDeltaTime = 0.01f;
+
+        ModelInit();
     }
 
     void Start()
     {
-        if (!isInitialized)
-        {
-            foreach (var bone in bones)
-            {
-                if (bone != null)
-                {
-                    body = bone.GetComponent<Rigidbody>();
-                    hinge = bone.GetComponent<HingeJoint>();
-                    collider = bone.GetComponent<CapsuleCollider>();
-
-                    controllers.Add(new Controller()
-                    {
-                        name = bone.name,
-                        targetPosition = (hinge!=null) ? hinge.spring.targetPosition : 0,
-                        isRoot = (hinge == null),
-                        bone = bone
-                    });
-
-                    totalVolume += (float) (Math.PI * Math.Pow(collider.radius, 2) *
-                                            (4f / 3f * collider.radius + collider.height));
-                }
-            }
-        }
+        CalculateMass();
     }
 
     void Update()
@@ -65,30 +63,77 @@ public class ModelController : MonoBehaviour
         UpdateModel();
     }
 
+    private void ModelInit()
+    {
+        if (parts.Length == 0)
+            parts = new float[bones.Length];
+
+        for (int i = 0; i < bones.Length; i++)
+        {
+            JointSpring spring = new JointSpring()
+            {
+                spring = (i==0)?modelSpring/2:modelSpring,
+                damper = modelDamper,
+                targetPosition = (useCustomValues)?parts[i]:bones[i].GetComponent<HingeJoint>().spring.targetPosition
+            };
+            bones[i].GetComponent<HingeJoint>().spring = spring;
+
+            var capsuleCollider = bones[i].GetComponent<CapsuleCollider>();
+            if(capsuleCollider != null) //Capsule Collider
+                totalVolume += (float) (Math.PI * Math.Pow(capsuleCollider.radius, 2) *
+                                    (4f / 3f * capsuleCollider.radius + capsuleCollider.height));
+            else
+            { //Box Collider
+                var boxCollider = bones[i].GetComponent<BoxCollider>();
+                totalVolume += boxCollider.size.x * boxCollider.size.y * boxCollider.size.z;
+            }
+        }
+    }
 
     private void UpdateModel()
     {
-        foreach (var controller in controllers)
+        for (int i = 0; i < bones.Length; i++)
         {
-            var radius = controller.bone.GetComponent<CapsuleCollider>().radius;
-            var height = controller.bone.GetComponent<CapsuleCollider>().height;
-            if (!controller.isRoot) // we don't have hing in root bone
+            JointSpring spring = new JointSpring()
             {
-                JointSpring spring = new JointSpring()
-                {
-                    spring = modelSpring,
-                    damper = modelDamper,
-                    targetPosition = controller.targetPosition,
-                };
-                controller.bone.GetComponent<HingeJoint>().spring = spring;
-            }
-            controller.bone.GetComponent<Rigidbody>().mass =
-                totalMass * (float) (Math.PI * Math.Pow(radius, 2) * (4f / 3f * radius + height)) / totalVolume;
+                spring = (i == 0) ? modelSpring / 2 : modelSpring,
+                damper = modelDamper,
+                targetPosition = parts[i]
+            };
+            bones[i].GetComponent<HingeJoint>().spring = spring;
         }
+    }
+
+    private void CalculateMass()
+    {
+        float radius;
+        float height;
+        foreach (var t in bones)
+        {
+            if (t.GetComponent<CapsuleCollider>() != null)
+            {
+                radius = t.GetComponent<CapsuleCollider>().radius;
+                height = t.GetComponent<CapsuleCollider>().height;
+
+                t.GetComponent<Rigidbody>().mass =
+                    totalMass * (float)(Math.PI * Math.Pow(radius, 2) * (4f / 3f * radius + height)) / totalVolume;
+            }
+            else
+            {
+                var size = t.GetComponent<BoxCollider>().size;
+                /*t.GetComponent<Rigidbody>().mass =
+                    totalMass * size.x*size.y*size.z / totalVolume;*/
+            }
+        }
+
+        radius = root.GetComponent<CapsuleCollider>().radius;
+        height = root.GetComponent<CapsuleCollider>().height;
+        root.GetComponent<Rigidbody>().mass =
+            totalMass * (float) (Math.PI * Math.Pow(radius, 2) * (4f / 3f * radius + height)) / totalVolume;
     }
 }
 
-[System.Serializable]
+/*[System.Serializable]
 public class Controller
 {
     [HideInInspector] public string name;
@@ -98,4 +143,14 @@ public class Controller
     [HideInInspector] public bool isRoot;
 
     public float targetPosition;
+}*/
+
+public class NamedArrayAttribute : PropertyAttribute
+{
+    public readonly string[] names;
+
+    public NamedArrayAttribute(string[] names)
+    {
+        this.names = names;
+    }
 }
