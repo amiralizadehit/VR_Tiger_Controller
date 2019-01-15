696acf89e0f439ee2a15e63c56ced66614e4b1e0
Amir Alizadeh <amiralizadeh.it@gmail.com>
2019-01-15 18:32:19 +0200

    Visualized Joints Anchor Positions

---
+++ Assets/Scripts/IKJoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKJoint : MonoBehaviour
+{
+
+    public HingeJoint hing;
+
+    [SerializeField] public Mesh Cylinder;
+
+
+	void Start ()
+    {
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var d = transform.TransformPoint(hing.anchor);
+        //(hing.angle);
+    }
+
+    private void OnDrawGizmos()
+    {
+        var position = transform.TransformPoint(hing.anchor);
+        Gizmos.color = new Color(Color.white.r, Color.white.g, Color.white.b, 0.5f);
+        Gizmos.DrawSphere(position,0.03f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(position,hing.connectedBody.transform.position);
+    }
+}
--- Assets/Scripts/VRInput.cs
+++ Assets/Scripts/VRInput.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR;
 
-public class VRInput : MonoBehaviour {
+public class VRInput : MonoBehaviour
+{
+
+    public GameObject RightHand;
+    public GameObject LeftHand;
+    public GameObject RightFoot;
+    public GameObject LeftFoot;
+
+
+    public Text RightHandText;
+    public Text LeftHandText;
+    public Text RightFootText;
+    public Text LeftFootText;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +25,13 @@ public class VRInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		RightHandText.text =
+            $"({RightHand.transform.position.x:0.###}, {RightHand.transform.position.y:0.###}, {RightHand.transform.position.z:0.###})";
+        LeftHandText.text =
+            $"({LeftHand.transform.position.x:0.###}, {LeftHand.transform.position.y:0.###}, {LeftHand.transform.position.z:0.###})";
+        RightFootText.text =
+            $"({RightFoot.transform.position.x:0.###}, {RightFoot.transform.position.y:0.###}, {RightFoot.transform.position.z:0.###})";
+        LeftFootText.text =
+            $"({LeftFoot.transform.position.x:0.###}, {LeftFoot.transform.position.y:0.###}, {LeftFoot.transform.position.z:0.###})";
+    }
 }
