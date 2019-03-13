519672c7218da140ebffc8ae7f34181c5b85b76c
Amir Alizadeh <amiralizadeh.it@gmail.com>
2019-03-13 18:25:36 +0200

    Applying IK angle rotation to actual model

---
+++ Assets/Scripts/AngleMapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleMapper : MonoBehaviour
+{
+
+    public List<HingeJoint> Hinges;
+    public AngleReturn angleObj;
+
+    private float[] angles;
+
+
+	void Start ()
+    {
+        
+
+    }
+	
+	
+	void Update ()
+    {
+        angles = angleObj.GetAngles();
+        for (int i = 0; i < angles.Length; i++)
+        {
+            var jointSpring = new JointSpring()
+            {
+                spring = Hinges[i].spring.spring,
+                damper = Hinges[i].spring.damper,
+                targetPosition = angles[i]
+            };
+            Hinges[i].spring = jointSpring;
+        }
+    }
+}
--- Assets/Scripts/AngleReturn.cs
+++ Assets/Scripts/AngleReturn.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class AngleReturn : MonoBehaviour
 {
+    // here we should set IK chain all in order, from parent to the last child.
     public List<Transform> Bones;
 
-    public Transform endEffector;
+    public bool isLeg;
 
     //here we keep track of initial vectors each joint makes with its child in IK chain (except last joint which does not have any child)
     private Vector3[] InitialVectors;
@@ -15,14 +16,13 @@ public class AngleReturn : MonoBehaviour
 
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
-        InitialVectors = new Vector3[Bones.Count-1];
-        Angles = new float[Bones.Count-1];
-      
+        InitialVectors = new Vector3[Bones.Count - 1];
+        Angles = new float[Bones.Count - 1];
+
         for (int i = 0; i < Bones.Count - 1; i++)
         {
-            print("hello");
             Vector3 vector = Bones[i + 1].position - Bones[i].position;
             InitialVectors[i] = vector;
         }
@@ -31,19 +31,41 @@ public class AngleReturn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < Bones.Count-1; i++)
+        for (int i = 0; i < Bones.Count - 1; i++)
         {
             Vector3 newVector = Bones[i + 1].position - Bones[i].position;
             //Vector3 toEF = endEffector.position - Bones[i].position;
 
             //Here we store the angle which init vector makes with the current vector (after IK update)
-            //we should also multiply it by the sign of their cross product in x axis to know when the angle is negative and when it's positive
+
+            //we should also multiply it by the sign of their cross product in x axis to know when the angle is negative and when it's positive.
+
+
+            var t = (isLeg) ? -1 : 1; //rotation sign is different for legs and hands
 
             Angles[i] = Vector3.Angle(InitialVectors[i],
-                newVector) * -Mathf.Sign(Vector3.Cross(InitialVectors[i],newVector).x);
+                            newVector) * Mathf.Sign(Vector3.Cross(InitialVectors[i], newVector).x) * t;
+
+            //we should also take parent rotation into consideration,
+            //hence, for each joint, we should subtract the rotation of all the joints above that to get its own pure rotation.
+
+            if (i == 0) continue; // if it's not the root itself
+            float parentValues = 0;
+            for (var j = 0; j < i; j++)
+            {
+                parentValues += Angles[j];
+            }
+
+            Angles[i] -= parentValues;
         }
 
-        Debug.Log(Angles[0]);
-        
+
+        // Debug.Log("Angle 0 :"+Angles[0]);
+        Debug.Log("Angle 1 :" + Angles[1]);
+    }
+
+    public float[] GetAngles()
+    {
+        return Angles;
     }
 }
